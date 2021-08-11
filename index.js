250fd161c81041a43771dbb4846b6f8e485bdc51
caseytansey <ctansey12600@outlook.com>
2021-08-11 14:19:28 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(someValue){
+    if(someValue <= '42'){
+        return 42 - someValue
+    }
+    else{
+        return someValue - 42
+    }
+}
+
+function distanceFromHqInFeet(someValue){
+    if(someValue <= '42'){
+        return (42 - someValue) * 264
+    }
+    else{
+        return (someValue - 42) * 264
+    }
+}
+
+let distanceTravelledInFeet = function(firstDistance,secondDistance){
+    if(firstDistance < secondDistance){
+        return (secondDistance - firstDistance) * 264
+    }else{
+        return (firstDistance - secondDistance) * 264
+    }
+}
+
+function calculatesFarePrice(distanceTravelledInFeet){
+    if(distanceTravelledInFeet < '400'){
+        return 0
+    }else if((distanceTravelledInFeet < '2000') && (distanceTravelledInFeet > '400')){
+        return (distanceTravelledInFeet * 2)
+    }
+}
